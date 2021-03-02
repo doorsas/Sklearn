2b927c65810f7543f9ec7d2a9001eb8ae5046662
Evaldas <doorsas@gmail.com>
2021-03-02 12:00:11 +0200

    prideta klase bandymas

--- main.py
+++ main.py
@@ -10,17 +10,38 @@ from sklearn.model_selection import train_test_split
 file_name = 'books_small.json'
 
 reviews = []
+bandymai = []
 
 class Sentiment:
     NEGATIVE = 'NEGATIVE'
     NEUTRAL = 'NEUTRAL'
     POSITIVE = 'POSITIVE'
 
+
+class Bandymas:
+    def __init__(self, text, score):
+        self.text = text
+        self.score = score
+        self.sentiment = self.get_sentiment()
+        self.zodis = self.text.split()
+
+    def get_sentiment(self):
+        if int(self.score) <= 2 :
+            return Sentiment.NEGATIVE
+        elif int(self.score) == 3 :
+            return Sentiment.NEUTRAL
+        else:
+            return Sentiment.POSITIVE
+
+
+
+
 class Review:
     def __init__(self, text, score):
         self.text = text
         self.score = score
         self.sentiment = self.get_sentiment()
+
 
     def get_sentiment(self):
         if int(self.score) <= 2 :
@@ -37,8 +58,18 @@ with open(file_name) as f:
         # print (rewiew['reviewText'])
         reviews.append(Review(review['reviewText'], review['overall']))
 
+with open(file_name) as f:
+    for line in f:
+        review = json.loads(line)
+        # print (rewiew['reviewText'])
+        bandymai.append(Bandymas(review['reviewText'] + '10',review['overall'] + 20 ))
+
+
+
 print (reviews[5].score)
 
 
 
-print (reviews[5].sentiment, reviews[5].text)
+# print (reviews[2].sentiment, reviews[2].score)
+
+print (bandymai[3].text, bandymai[3].zodis[:3])
